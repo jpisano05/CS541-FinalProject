80f2c21ba7ef53ce740468c936a3bbe84f7d45a3
jpisano05 <jjpisano@wpi.edu>
2026-04-20 15:46:35 -0400

    removed unnecessary pieces

--- train.py
+++ train.py
@@ -1,2 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader, Dataset
+from torchvision import datasets, models
+from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+from tempfile import TemporaryDirectory
 import os
+import time
 
